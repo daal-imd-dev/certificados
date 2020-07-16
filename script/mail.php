c52f49f24688da9272f7545bb865d067c8a91f1d
MoisesMsa <moises78zito@Hotmail.com>
2020-07-16 08:43:47 -0300

    grouping before and after attchment

--- script/mail.php
+++ script/mail.php
@@ -31,13 +31,12 @@
 			
 			if ($email == $current_email){
 				$mail->addAttachment(DESTINE.$file_name, $new_file_name);
-				continue; 
 			}else{
+				$mail->addAddress(TEST_EMAIL);		
 				$mail->send();		
 				$email = $current_email;
 				$mail->ClearAllRecipients(); 
 				$mail->clearAttachments();
-				$mail->addAddress(TEST_EMAIL);		
 				$mail->addAttachment(DESTINE.$file_name, $new_file_name);  
 			}
 		}
