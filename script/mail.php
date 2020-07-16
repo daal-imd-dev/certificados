319cac686815d1b42a84a7e98900dc29f3dc81f1
MoisesMsa <moises78zito@Hotmail.com>
2020-07-16 13:55:54 -0300

    template adjusts

--- script/mail.php
+++ script/mail.php
@@ -41,22 +41,3 @@
 	}
 
 
-	function send_emails_debug($from="", $title="", $body_file=""){
-		$files = get_files();
-		$file_name = $files[0];
-		$email = explode("#",$files[0]);
-		$new_file_name = $email[1];
-		$email = $email[0];
-		
-		global $mail;
-		
-		$mail->Subject = $title;
-		$mail->Body    = file_get_contents($body_file);
-		$mail->SetFrom($from);
-		$mail->Username = SENDER_EMAIL;
-		$mail->addAddress(TEST_EMAIL);
-		$mail->addAttachment(DESTINE.$file_name, $new_file_name);
-		echo var_dump($mail);
-		$mail->send() ? "enviado" : $mail->ErrorInfo;
-	}
-
